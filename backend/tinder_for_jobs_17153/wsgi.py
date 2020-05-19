63311ea4a98fd4b99c27ce85eb90bfcc1a2c1a36
Crowdbotics <team@crowdbotics.com>
2020-05-19 18:20:56 +0000

    Update models

--- backend/tinder_for_jobs_17153/wsgi.py
+++ backend/tinder_for_jobs_17153/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tinder_for_jobs_17153.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tinder_for_jobs_17153.settings")
 
 application = get_wsgi_application()
