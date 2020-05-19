cc1a0f675e27252a103e1b4c55b1ff6cdc579f97
Crowdbotics <team@crowdbotics.com>
2020-05-19 19:24:20 +0000

    add package blueprint-react-native-sign-in-2-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn248161Navigator from '../features/SignIn248161/navigator';
 import Settings48106Navigator from '../features/Settings48106/navigator';
 import UserProfile48099Navigator from '../features/UserProfile48099/navigator';
 import Settings48098Navigator from '../features/Settings48098/navigator';
@@ -26,6 +27,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+SignIn248161: { screen: SignIn248161Navigator },
 Settings48106: { screen: Settings48106Navigator },
 UserProfile48099: { screen: UserProfile48099Navigator },
 Settings48098: { screen: Settings48098Navigator },
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import SignIn248161Reducer from '../features/SignIn248161/redux/reducers'
 import SignIn248094Reducer from '../features/SignIn248094/redux/reducers'
 import CalendarReducer from '../features/Calendar/redux/reducers';
 import EmailAuthReducer from '../features/EmailAuth/redux/reducers';
@@ -18,6 +19,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+SignIn248161: SignIn248161Reducer,
 SignIn248094: SignIn248094Reducer,
 Calendar: CalendarReducer,
 EmailAuth: EmailAuthReducer,
